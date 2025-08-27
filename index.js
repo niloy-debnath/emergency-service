e910fb5b1e27333f334e862ec45522ca8d7209ec
Niloy Debnath <niloydebnathofficial@gmail.com>
2025-08-27 16:12:03 +0600

    coin deduction for calls is applied

--- index.js
+++ index.js
@@ -2,16 +2,37 @@
 
 const heartIcons = document.getElementsByClassName("heart-png");
 let navHeartElement = document.getElementById("nav-heart-number");
-console.log(navHeartElement);
 
 for (let icon of heartIcons) {
   icon.addEventListener("click", function () {
     let currentValue = parseInt(navHeartElement.innerText);
 
     currentValue++;
-    console.log(currentValue);
+
     navHeartElement.innerText = currentValue;
   });
 }
 
-//
+//----- Call Button --------
+
+const callButtons = document.getElementsByClassName("call-button");
+
+for (let callButton of callButtons) {
+  callButton.addEventListener("click", function () {
+    let cardTitle =
+      callButton.parentNode.parentNode.childNodes[3].childNodes[1].innerText;
+    let number =
+      callButton.parentNode.parentNode.childNodes[5].childNodes[1].innerText;
+    // console.log(number);
+    alert("You clicked " + cardTitle + " and calling on " + number);
+
+    // coin count
+
+    let totalCoinElement = document.getElementById("total-coin");
+    let totalCoin = parseInt(totalCoinElement.innerText);
+    // console.log(totalCoin);
+    let currentCoin = totalCoin - 20;
+    totalCoinElement.innerText = currentCoin;
+    console.log(currentCoin);
+  });
+}
