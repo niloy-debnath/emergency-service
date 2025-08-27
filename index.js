39efc4423ba8e5fe9caef73e6db7821eda85e109
Niloy Debnath <niloydebnathofficial@gmail.com>
2025-08-27 21:39:26 +0600

    clear button and js added

--- index.js
+++ index.js
@@ -18,27 +18,57 @@ for (let icon of heartIcons) {
 const callButtons = document.getElementsByClassName("call-button");
 
 for (let callButton of callButtons) {
+  // Click Event
+
   callButton.addEventListener("click", function () {
     let cardTitle =
       callButton.parentNode.parentNode.childNodes[3].childNodes[1].innerText;
+
     let number =
       callButton.parentNode.parentNode.childNodes[5].childNodes[1].innerText;
-    // console.log(number);
+
     let totalCoinElement = document.getElementById("total-coin");
+
     let totalCoin = parseInt(totalCoinElement.innerText);
+
     if (totalCoin < 20) {
       alert("You don't have sufficient coins");
       return;
     }
 
-    // console.log(totalCoin);
     let currentCoin = totalCoin - 20;
 
     totalCoinElement.innerText = currentCoin;
-    alert("You clicked " + cardTitle + " and calling on " + number);
+    alert("Calling " + cardTitle + " " + number + " you have " + totalCoin);
 
-    // coin count
+    let historyCardContainer = document.getElementById(
+      "history-card-container"
+    );
+    console.log(historyCardContainer);
 
-    console.log(currentCoin);
+    const newCallHistory = document.createElement("div");
+
+    newCallHistory.innerHTML = `
+       <div
+            class="bg-[#FAFAFA] p-4 rounded-lg flex justify-between mt-3 items-center"
+          >
+            <div>
+              <h1 class="font-semibold">${cardTitle}</h1>
+              <h2 class="text-[#5C5C5C] hind-madurai-regular">${number}</h2>
+            </div>
+            <div>
+              <h1>Time</h1>
+            </div>
+          </div>
+    `;
+
+    historyCardContainer.append(newCallHistory);
   });
 }
+
+const clearButton = document.getElementById("clear-btn");
+
+clearButton.addEventListener("click", function () {
+  let historyCardContainer = document.getElementById("history-card-container");
+  historyCardContainer.innerHTML = "";
+});
